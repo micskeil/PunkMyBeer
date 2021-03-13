12bb91f854698438e99c198d6c3feddc799ab17e
micskeil <micskeil@gmail.com>
2021-03-13 22:22:32 +0100

    filer finished

--- src/components/Beer.vue
+++ src/components/Beer.vue
@@ -22,11 +22,16 @@
     class="flex justify-center bg-main bg-cover bg-top w-screen overflow-hidden h-screen overflow-y-hidden "
   >
     <div
-      class="bg-yellow-500 flex flex-row mx-2 my-auto  md:m-auto opacity-90 border-yellow-700 border-2 z-50 rounded-md fit-content font-base max-w-xl shadow text-gray-100"
+      class="bg-yellow-500 mx-2 my-auto  md:m-auto opacity-90 border-yellow-700 border-2 z-50 rounded-md fit-content font-base max-w-xl shadow text-gray-100"
     >
-      <div class="self-center " v-on:click="previousBeer">
+      <div
+        class="flex flex-col border-yellow-700 border-t-2 p-2 md:px-4  text-base bg-yellow-700 text-gray-100 capitalize justify-center items-center"
+      >
+        <Filter v-on:filter="filterBeer" v-if="isFilterActive" />
+
         <svg
-          class="w-10"
+          v-on:click="toggleFilter"
+          class="w-4 animate-bounce "
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
@@ -36,87 +41,82 @@
             stroke-linecap="round"
             stroke-linejoin="round"
             stroke-width="2"
-            d="M15 19l-7-7 7-7"
+            d="M19 9l-7 7-7-7"
           />
         </svg>
       </div>
-      <div>
-        <div
-          class="pt-5 text-4xl text-gray-100 px-4 md:px-8 capitalize font-title flex items-center justify-center"
-          v-if="!isFilterActive"
-        >
-          <div>{{ beer[activeBeer].name }}</div>
-        </div>
-        <div
-          class="pt-5 text-4xl text-gray-100 px-4 md:px-8 capitalize font-title flex items-center justify-center"
-        >
-          <div v-if="isFilterActive" class="mb-4 flex">
-            <input
-              class="rounded-md text-gray-900 p-2"
-              type="text"
-              name=""
-              id=""
-              v-model="name"
+
+      <div class="flex flex-row">
+        <div class="self-center " v-on:click="previousBeer">
+          <svg
+            class="w-10"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M15 19l-7-7 7-7"
             />
-            <svg
-              class=" w-16 animate-pulse"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              v-on:click="fetchAllBeer"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M13 5l7 7-7 7M5 5l7 7-7 7"
-              />
-            </svg>
+          </svg>
+        </div>
+        <div>
+          <div
+            class="pt-5 text-4xl text-gray-100 px-4 md:px-8 capitalize font-title flex items-center justify-center"
+          >
+            <div>
+              {{ activeBeer + 1 }}/{{ this.beer.length }}
+              <div>{{ beer[activeBeer].name }}</div>
+            </div>
+          </div>
+          <div
+            class="pt-5 text-4xl text-gray-100 px-4 md:px-8 capitalize font-title flex items-center justify-center"
+          ></div>
+          <div
+            class="grid grid-cols-1 pt-5 text-xl text-white capitalize px-4 md:px-8 py-4 md:py-8 text-justify "
+          >
+            <div class="capitalize">{{ beer[activeBeer].description }}</div>
+          </div>
+          <div
+            class="grid grid-cols-4 border-yellow-700 border-t-2 p-2 md:px-4 rounded-t-md   text-xl bg-yellow-700 text-gray-100 capitalize"
+          >
+            <div class="">
+              <div class="">ABV</div>
+              <div>{{ beer[activeBeer].abv }}%</div>
+            </div>
+            <div>
+              <div>IBU</div>
+              <div>{{ beer[activeBeer].ibu }}</div>
+            </div>
+            <div>
+              <div>EBC</div>
+              <div>{{ beer[activeBeer].ebc }}</div>
+            </div>
+            <div>
+              <div>PH</div>
+              <div>{{ beer[activeBeer].ph }}</div>
+            </div>
           </div>
         </div>
-        <div
-          v-if="!isFilterActive"
-          class="grid grid-cols-1 pt-5 text-xl text-white capitalize px-4 md:px-8 py-4 md:py-8 text-justify "
-        >
-          <div class="capitalize">{{ beer[activeBeer].description }}</div>
+        <div class=" self-center" v-on:click="nextBeer">
+          <svg
+            class="w-10"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9 5l7 7-7 7"
+            />
+          </svg>
         </div>
-        <div
-          class="grid grid-cols-4 border-yellow-700 border-t-2 p-2 md:px-4 rounded-t-md   text-xl bg-yellow-700 text-gray-100 capitalize"
-        >
-          <div class="">
-            <div class="">ABV</div>
-            <div>{{ beer[activeBeer].abv }}%</div>
-          </div>
-          <div>
-            <div>IBU</div>
-            <div>{{ beer[activeBeer].ibu }}</div>
-          </div>
-          <div>
-            <div>EBC</div>
-            <div>{{ beer[activeBeer].ebc }}</div>
-          </div>
-          <div>
-            <div>PH</div>
-            <div>{{ beer[activeBeer].ph }}</div>
-          </div>
-        </div>
-      </div>
-      <div class=" self-center" v-on:click="nextBeer">
-        <svg
-          class="w-10"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M9 5l7 7-7 7"
-          />
-        </svg>
       </div>
     </div>
 
@@ -130,16 +130,16 @@
 
 <script>
 import axios from "axios";
+import Filter from "./Filter";
 
 export default {
-  components: {},
+  components: { Filter },
 
   data() {
     return {
       beer: [],
       activeBeer: 0,
-      isFilterActive: true,
-      name: "",
+      isFilterActive: false,
     };
   },
   computed: {
@@ -152,6 +152,35 @@ export default {
   },
 
   methods: {
+    toggleFilter() {
+      this.isFilterActive = !this.isFilterActive;
+    },
+
+    filterBeer(filter) {
+      const fixed_name = filter.name.replaceAll(" ", "_");
+
+      const name = filter.name ? `&beer_name=${fixed_name}` : ``;
+
+      const abv_gt = `&abv_gt=${filter.abv[0]}`;
+      const abv_lt = `&abv_lt=${filter.abv[1]}`;
+
+      const ibu_gt = `&ibu_gt=${filter.ibu[0]}`;
+      const ibu_lt = `&ibu_lt=${filter.ibu[1]}`;
+
+      const ebc_gt = `&ebc_gt=${filter.ebc[0]}`;
+      const ebc_lt = `&ebc_lt=${filter.ebc[1]}`;
+
+      axios
+        .get(
+          `https://api.punkapi.com/v2/beers??page=1&per_page=80${name}${abv_gt}${abv_lt}${ibu_gt}${ibu_lt}${ebc_gt}${ebc_lt}`
+        )
+        .then((response) => {
+          this.beer = response.data;
+          this.activeBeer = 0;
+          this.isFilterActive = false;
+        });
+    },
+
     fetchRandomBeer() {
       axios.get("https://api.punkapi.com/v2/beers/random").then((response) => {
         this.beer = response.data;
@@ -159,12 +188,11 @@ export default {
     },
 
     fetchAllBeer() {
-      console.log(this.name);
+      console.log(this.filter.name);
 
       axios
-        .get(`https://api.punkapi.com/v2/beers?beer_name=${this.name}`)
+        .get(`https://api.punkapi.com/v2/beers?beer_name=${this.filter.name}`)
         .then((response) => {
-          console.log(response.data);
           this.beer = response.data;
           this.activeBeer = Math.floor(Math.random() * this.beer.length);
           this.isFilterActive = false;
@@ -172,12 +200,11 @@ export default {
     },
 
     nextBeer() {
-      if (this.beer.length > 1 && this.activeBeer - 1 < this.beer.length) {
-        this.activeBeer++;
-        return;
+      if (this.beer.length > 1 && this.activeBeer + 1 < this.beer.length) {
+        return this.activeBeer++;
       } else if (
         this.beer.length > 1 &&
-        this.activeBeer - 1 === this.beer.length
+        this.activeBeer + 1 === this.beer.length
       ) {
         return;
       }
@@ -185,7 +212,7 @@ export default {
     },
 
     previousBeer() {
-      if (this.beer.length > 1 && this.activeBeer > 1) {
+      if (this.beer.length > 1 && this.activeBeer > 0) {
         this.activeBeer--;
         return;
       } else if (this.beer.length > 1 && this.activeBeer === 1) {
--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -1,19 +1,51 @@
 <template>
-  <div
-    class="w-screen h-screen bg-gray-800 flex justify-center items-center text-gray-900"
-  >
-    <div class="w-96 h-64 bg-gray-100 rounded-md">
-      <input type="text" class="w-4/5 m-2 bg-red-200" />
+  <div class="flex flex-col justify-start w-full font-title text-xl">
+    <input
+      type="text"
+      placeholder="Search for name..."
+      v-model="filter.name"
+      class="m-2 bg-gray-100 rounded text-2xl p-2 text-gray-900 font-display"
+    />
+
+    <label class="mt-5" for="ABV">ABV</label>
+    <Slider id="abv-slider" :max="67.5" v-model="filter.abv" />
+
+    <label class="mt-5" for="IBU">IBU</label>
+    <Slider id="ibu-slider" :max="120" v-model="filter.ibu" />
+    <label class="mt-5" for="EBC">EBC</label>
+    <Slider id="ebc-slider" :max="138" v-model="filter.ebc" />
+
+    <div
+      v-on:click="filterBeer"
+      class="shadow-md bg-gray-100 text-gray-900 my-5 :hover:bg-gray-100 rounded-md cursor-pointer"
+    >
+      Find my beer
     </div>
   </div>
 </template>
 
 <script>
+import Slider from "@vueform/slider";
+
 export default {
+  components: { Slider },
+
   data() {
-    return {};
+    return {
+      filter: {
+        name: "",
+        abv: [0, 25],
+        ibu: [5, 120],
+        ebc: [4, 130],
+      },
+    };
+  },
+  methods: {
+    filterBeer() {
+      this.$emit("filter", this.filter);
+    },
   },
 };
 </script>
 
-<style></style>
+<style src="@vueform/slider/themes/default.css"></style>
